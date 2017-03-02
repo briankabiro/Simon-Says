c6da51bbb36d310bb7a2a01784c0f84a95b95615
Brian Kabiro <briankigondu@gmail.com>
2017-03-02 21:48:32 +0300

    future design

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
+/*
+	array with last used values
+	array with all of the computed values
+	computed function to calculate the next one(generate a random number between 1 and 4)
+	function to change opacity
+	function to keep track of the inputed values and check against	
+	click, the game starts from one. it highlights that one and saves it in that array
+	keeps track of user clicks and then checks it after
+*/
 var app = new Vue({
 	el:'.container',
-	data:{},
+	data:{
+		recentArray:[],
+		eachArray:[]
+	},
 	methods: {
 		startGame: function(event){
-			alert('Hello' + event.target.tagName);
+			console.log('Game has started');
+
+			
 		}
 	}
 })
